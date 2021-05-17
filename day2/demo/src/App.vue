bb3af0a40b9278177b01d2a6b4c4ef271ee8547b
chenyang <chenyang_nn@163.com>
2021-05-17 16:47:42 +0800

    父子组件通信(props $emit)

--- day2/demo/src/App.vue
+++ day2/demo/src/App.vue
@@ -1,23 +1,19 @@
 <template>
   <div id="app">
-    <Parent />
+    <img src="./assets/logo.png">
+    <router-view/>
   </div>
 </template>
 
 <script>
-import Parent from "./components/Parent";
-
 export default {
-  name: "App",
-  components: {
-    Parent,
-  },
-};
+  name: 'App'
+}
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
---
+++ day2/demo/src/components/Count.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <button v-on:click="increment">点击加1</button>
+    <button @click="decrement">点击减1</button>
+    <p>
+      <span>{{ num }}</span>
+    </p>
+  </div>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+    }
+  },
+  props: ['num'],
+  methods: {
+    increment () {
+      this.$emit('incre')
+    },
+    decrement () {
+      this.$emit('decre')
+    }
+  }
+}
+</script>
+<style>
+</style>
